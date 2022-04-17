4d90415f79dcbf03186c4bbfa771970229b099f1
purplevarun <purplevarun@gmail.com>
2022-04-17 22:49:04 +0530

    Extracted Router.js from App.js.

--- App.js
+++ App.js
@@ -1,30 +1,5 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import CreateReminderScreen from "./screens/CreateReminderScreen";
-import MyRemindersScreen from "./screens/MyRemindersScreen";
-import SettingsScreen from "./screens/SettingsScreen";
+import Router from "./router/Router";
 const App = () => {
-	const Stack = createNativeStackNavigator();
-	return (
-		<NavigationContainer>
-			<Stack.Navigator
-				screenOptions={{ headerShown: false }}
-				initialRouteName="Create Reminder"
-			>
-				<Stack.Screen
-					name="Create Reminder"
-					component={CreateReminderScreen}
-				/>
-				<Stack.Screen
-					name="My Reminders"
-					component={MyRemindersScreen}
-				/>
-				<Stack.Screen
-					name="Settings Screen"
-					component={SettingsScreen}
-				/>
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+	return <Router />;
 };
 export default App;
---
+++ router/Router.js
@@ -0,0 +1,30 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CreateReminderScreen from "./../screens/CreateReminderScreen";
+import MyRemindersScreen from "./../screens/MyRemindersScreen";
+import SettingsScreen from "./../screens/SettingsScreen";
+const Router = () => {
+	const Stack = createNativeStackNavigator();
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				screenOptions={{ headerShown: false }}
+				initialRouteName="Create Reminder"
+			>
+				<Stack.Screen
+					name="Create Reminder"
+					component={CreateReminderScreen}
+				/>
+				<Stack.Screen
+					name="My Reminders"
+					component={MyRemindersScreen}
+				/>
+				<Stack.Screen
+					name="Settings Screen"
+					component={SettingsScreen}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+export default Router;
